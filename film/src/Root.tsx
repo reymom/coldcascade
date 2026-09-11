ee0c6eef0ade2e1f17eb0248ed525d937e60611a
reymom <ramon_gs95@hotmail.com>
2026-09-11 03:13:26 +0200

    the shipped composition is the one on the page

--- film/src/Root.tsx
+++ film/src/Root.tsx
@@ -1,11 +1,9 @@
 import { Composition } from "remotion";
-import { Film } from "./Film";
 import { Mechanism, DURATION, FPS } from "./Mechanism";
 
 export const Root: React.FC = () => (
   <>
     <Composition id="Mechanism" component={Mechanism} durationInFrames={DURATION} fps={FPS} width={1920} height={1080} />
     {/* v4, kept for the archive */}
-    <Composition id="Film" component={Film} durationInFrames={2400} fps={60} width={1920} height={1080} />
   </>
 );
